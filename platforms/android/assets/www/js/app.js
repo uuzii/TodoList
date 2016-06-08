9046883d5d147ed585b81b022ab16e399a2f7cc1
uuzii <rodriguez.uzi@gmail.com>
2016-06-08 11:26:43 -0500

    lista funcional

---
+++ platforms/android/assets/www/js/app.js
@@ -0,0 +1,29 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'firebase'])
+
+.factory('Items', ['$firebaseArray', function($firebaseArray){
+	var itemsRef = new Firebase('https://pruebaionicfirebase1.firebaseio.com/items');
+	return $firebaseArray(itemsRef);
+}])
+
+.controller('ListCtrl', function($scope, $ionicListDelegate, Items){
+    $scope.items = Items;
+    $scope.addItem = function(){
+      var name = prompt('What do you need to buy?');
+      if (name) {
+        $scope.items.$add({'name': name});
+      }
+    };
+
+$scope.purchaseItem = function(item){
+	$scope.item = item;
+	$scope.item['status'] = 'purchased';
+	$ionicListDelegate.closeOptionButtons();
+	};
+});
